9483e933e225ccf785615729b2b53c45c0fade06
Romário Ferreira <romarioferreirarezende@hotmail.com>
2022-02-09 21:07:02 -0300

    tipos curinga

---
+++ tipos-curinga/src/program/Application.java
@@ -0,0 +1,23 @@
+package program;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+	
+	public static void main(String[] args) {
+		List<Integer> myInts = Arrays.asList(5,2,10);
+		printList(myInts);
+		
+		List<String> myStrs = Arrays.asList("Maria","Alex","Bob");
+		printList(myStrs);
+	}
+
+	private static void printList(List<?> list) {
+		for(Object obj: list) {
+			System.out.println(obj);
+		}
+		
+	}
+
+}
